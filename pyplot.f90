3b186d542b096882dcb1f7320fefb925a881818a
GasinAn <Gasin185@163.com>
2022-04-27 18:36:23 +0800

    add add_real32_data & add_real128_data

--- pyplot.f90
+++ pyplot.f90
@@ -1,6 +1,6 @@
 module pyplot
 
-    use iso_fortran_env, only: dp => real64
+    use iso_fortran_env, only: real32, real64, real128
 
     implicit none
     private
@@ -9,8 +9,12 @@ module pyplot
     public :: plt_init, plt_add_data
 
     interface plt_add_data
-        module procedure plt_add_1d_data
-        module procedure plt_add_2d_data
+        module procedure plt_add_1d_real32_data
+        module procedure plt_add_1d_real64_data
+        module procedure plt_add_1d_real128_data
+        module procedure plt_add_2d_real32_data
+        module procedure plt_add_2d_real64_data
+        module procedure plt_add_2d_real128_data
     end interface plt_add_data
 
     contains
@@ -22,9 +26,9 @@ module pyplot
             close(10)
         end subroutine plt_init
 
-        subroutine plt_add_1d_data(py_array_name, fortran_array)
+        subroutine plt_add_1d_real32_data(py_array_name, fortran_array)
             character(*), intent(in) :: py_array_name
-            real(dp), intent(in) :: fortran_array(:)
+            real(real32), intent(in) :: fortran_array(:)
 
             character(txt_name_len) :: txt_name
             integer :: py_cmd_len
@@ -45,11 +49,61 @@ module pyplot
 
             txt_name_len= txt_name_len + 1
 
-        end subroutine plt_add_1d_data
+        end subroutine plt_add_1d_real32_data
 
-        subroutine plt_add_2d_data(py_array_name, fortran_array)
+        subroutine plt_add_1d_real64_data(py_array_name, fortran_array)
             character(*), intent(in) :: py_array_name
-            real(dp), intent(in) :: fortran_array(:,:)
+            real(real64), intent(in) :: fortran_array(:)
+
+            character(txt_name_len) :: txt_name
+            integer :: py_cmd_len
+            character(:), allocatable :: py_cmd
+
+            txt_name = '_'
+            open(10, file=txt_name//'.txt')
+            write(10,*) fortran_array
+            close(10)
+
+            open(10, file=py_file_name, access='append')
+            py_cmd_len = len(py_array_name) + 15 + len(txt_name) + 6
+            allocate(character(py_cmd_len) :: py_cmd)
+            py_cmd = py_array_name//" = np.loadtxt('"//txt_name//".txt')"
+            write(10,'(A)') py_cmd
+            deallocate(py_cmd)
+            close(10)
+
+            txt_name_len= txt_name_len + 1
+
+        end subroutine plt_add_1d_real64_data
+
+        subroutine plt_add_1d_real128_data(py_array_name, fortran_array)
+            character(*), intent(in) :: py_array_name
+            real(real128), intent(in) :: fortran_array(:)
+
+            character(txt_name_len) :: txt_name
+            integer :: py_cmd_len
+            character(:), allocatable :: py_cmd
+
+            txt_name = '_'
+            open(10, file=txt_name//'.txt')
+            write(10,*) fortran_array
+            close(10)
+
+            open(10, file=py_file_name, access='append')
+            py_cmd_len = len(py_array_name) + 15 + len(txt_name) + 6
+            allocate(character(py_cmd_len) :: py_cmd)
+            py_cmd = py_array_name//" = np.loadtxt('"//txt_name//".txt')"
+            write(10,'(A)') py_cmd
+            deallocate(py_cmd)
+            close(10)
+
+            txt_name_len= txt_name_len + 1
+
+        end subroutine plt_add_1d_real128_data
+
+        subroutine plt_add_2d_real32_data(py_array_name, fortran_array)
+            character(*), intent(in) :: py_array_name
+            real(real32), intent(in) :: fortran_array(:,:)
 
             integer :: i
             character(txt_name_len) :: txt_name
@@ -75,6 +129,66 @@ module pyplot
 
             txt_name_len= txt_name_len + 1
 
-        end subroutine plt_add_2d_data
+        end subroutine plt_add_2d_real32_data
+
+        subroutine plt_add_2d_real64_data(py_array_name, fortran_array)
+            character(*), intent(in) :: py_array_name
+            real(real64), intent(in) :: fortran_array(:,:)
+
+            integer :: i
+            character(txt_name_len) :: txt_name
+            integer :: fortran_array_shape(2)
+            integer :: py_cmd_len
+            character(:), allocatable :: py_cmd
+
+            txt_name = '_'
+            open(10, file=txt_name//'.txt')
+            fortran_array_shape = shape(fortran_array)
+            do i = 1, fortran_array_shape(1)
+                write(10,*) fortran_array(i,:)
+            end do
+            close(10)
+
+            open(10, file=py_file_name, access='append')
+            py_cmd_len = len(py_array_name) + 15 + len(txt_name) + 6
+            allocate(character(py_cmd_len) :: py_cmd)
+            py_cmd = py_array_name//" = np.loadtxt('"//txt_name//".txt')"
+            write(10,'(A)') py_cmd
+            deallocate(py_cmd)
+            close(10)
+
+            txt_name_len= txt_name_len + 1
+
+        end subroutine plt_add_2d_real64_data
+
+        subroutine plt_add_2d_real128_data(py_array_name, fortran_array)
+            character(*), intent(in) :: py_array_name
+            real(real128), intent(in) :: fortran_array(:,:)
+
+            integer :: i
+            character(txt_name_len) :: txt_name
+            integer :: fortran_array_shape(2)
+            integer :: py_cmd_len
+            character(:), allocatable :: py_cmd
+
+            txt_name = '_'
+            open(10, file=txt_name//'.txt')
+            fortran_array_shape = shape(fortran_array)
+            do i = 1, fortran_array_shape(1)
+                write(10,*) fortran_array(i,:)
+            end do
+            close(10)
+
+            open(10, file=py_file_name, access='append')
+            py_cmd_len = len(py_array_name) + 15 + len(txt_name) + 6
+            allocate(character(py_cmd_len) :: py_cmd)
+            py_cmd = py_array_name//" = np.loadtxt('"//txt_name//".txt')"
+            write(10,'(A)') py_cmd
+            deallocate(py_cmd)
+            close(10)
+
+            txt_name_len= txt_name_len + 1
+
+        end subroutine plt_add_2d_real128_data
 
 end module pyplot
